22eed1ed634bab06079697864001d9beec7606e5
Den-0786 <dennisopokuamponsah86@gmail.com>
2026-08-22 20:20:22 +0000

    Update district profile default email to ypgahinsandistrict@gmail.com

--- backend/core/management/commands/setup_credentials.py
+++ backend/core/management/commands/setup_credentials.py
@@ -18,7 +18,7 @@ class Command(BaseCommand):
                 defaults={
                     'first_name': 'District',
                     'last_name': 'Administrator',
-                    'email': 'ahinsandistrictypg@gmail.com',
+                    'email': 'ypgahinsandistrict@gmail.com',
                     'is_staff': True,
                     'is_superuser': False,
                 }
